7dbad194cbc4742d45513eaf8649951b5a4810f8
nayem islam <gangsn490@gmail.com>
2023-04-03 02:16:56 +0600

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 dotenv.config();
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
-import videoRoutes from "./routes/video.js";
-import commentRoutes from "./routes/comment.js";
-import cockieParser from "cookie-parser";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import videoRoutes from './routes/video.js';
+import commentRoutes from './routes/comment.js';
+import cockieParser from 'cookie-parser';
 
 const app = express();
 
@@ -15,7 +15,7 @@ const connect = () => {
   mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
-      console.log("db connectd");
+      console.log('db connectd');
     })
     .catch((err) => {
       throw err;
@@ -26,15 +26,15 @@ app.use(cockieParser());
 app.use(express.json());
 
 //ROUTES
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/videos", videoRoutes);
-app.use("/api/comments", commentRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/videos', videoRoutes);
+app.use('/api/comments', commentRoutes);
 
 //ERROR HANDLE
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const massage = err.massage || "Opps! Wrong";
+  const massage = err.massage || 'Opps! Wrong';
   return res.status(status).json({
     succsess: false,
     status,
@@ -45,5 +45,5 @@ app.use((err, req, res, next) => {
 //SERVER RUN
 app.listen(8800, () => {
   connect();
-  console.log("server runing 8800");
+  console.log('server runing 8800');
 });
